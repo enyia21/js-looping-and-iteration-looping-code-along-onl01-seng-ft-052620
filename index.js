659e17072d50bf3301f4ed41d49ca42f5d3a61c0
chibuisi <enyia21@gmail.com>
2020-09-11 16:05:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, message) {
+    const thankYouMessages = [];
+    for(let i=0; i<names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`);
+    }
+    return thankYouMessages;
+}
+
+function countDown(num) {
+    let i = num;
+    while(i >= 0) {
+        console.log(i);
+        i--;
+    }
+
+}
